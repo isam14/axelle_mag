2920a994caaede718f01fae656b31bd3aad49b1f
Rdyx <tiranico.pro@gmail.com>
2018-01-10 16:55:26 +0100

    index category article

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -16,4 +16,21 @@ class DefaultController extends Controller
         // replace this example code with whatever you need
         return $this->render('default/index.html.twig');
     }
+
+    /**
+     * @Route ("categorie/{id}", name="categorie")
+     */
+
+    public function categorieAction()
+    {
+        return $this->render('default/categorie.html.twig');
+    }
+    /**
+     * @Route("categorie/{catId}/article/{id}", name="article")
+     */
+
+    public function articleAction()
+    {
+        return $this->render('default/article.html.twig');
+    }
 }
