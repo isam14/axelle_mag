62864dafa503e861da4463e3b2ba121b9e7e662c
Rdyx <tiranico.pro@gmail.com>
2018-01-22 13:52:53 +0100

    mentions + fontsize ckeditor

---
+++ src/AppBundle/Entity/Mentions.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace AppBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Mentions
+ *
+ * @ORM\Table(name="mentions")
+ * @ORM\Entity(repositoryClass="AppBundle\Repository\MentionsRepository")
+ */
+class Mentions
+{
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="content", type="text")
+     */
+    private $content;
+
+
+    /**
+     * Get id.
+     *
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set content.
+     *
+     * @param string $content
+     *
+     * @return Mentions
+     */
+    public function setContent($content)
+    {
+        $this->content = $content;
+
+        return $this;
+    }
+
+    /**
+     * Get content.
+     *
+     * @return string
+     */
+    public function getContent()
+    {
+        return $this->content;
+    }
+}
