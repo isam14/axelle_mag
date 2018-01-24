66cb1762dc596cb0b9c200c9fe6e73b8ebc00bb5
Rdyx <tiranico.pro@gmail.com>
2018-01-24 10:35:41 +0100

    fix sql

--- initialisationLignesTables.sql
+++ initialisationLignesTables.sql
@@ -1,10 +1,10 @@
-INSERT INTO contact ('content') VALUES ('start');
+INSERT INTO contact (content) VALUES ('start');
 
-INSERT INTO newsletter ('email') VALUES ('start');
+INSERT INTO newsletter (email) VALUES ('start');
 
-INSERT INTO selected_article ('nom') VALUES ('Menu 3');
-INSERT INTO selected_article ('nom') VALUES ('Menu 2');
-INSERT INTO selected_article ('nom') VALUES ('Menu 1');
-INSERT INTO selected_article ('nom') VALUES ('Index');
+INSERT INTO selected_article (nom) VALUES ('Menu 3');
+INSERT INTO selected_article (nom) VALUES ('Menu 2');
+INSERT INTO selected_article (nom) VALUES ('Menu 1');
+INSERT INTO selected_article (nom) VALUES ('Index');
 
-INSERT INTO site_info ('apropos') VALUES ('start');
+INSERT INTO site_info (apropos, facebook, twitter, instagram, mail) VALUES ('start','start','start','start','start');
